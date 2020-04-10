174d02cba9de71124dbd592f88409d3cf94567d2
sasaki kei <sasakei1021@icloud.com>
2020-04-10 10:28:11 +0900

    deploy-production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
+server '18.180.125.117', user: 'sasakei', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
@@ -41,6 +42,7 @@
 #
 # Global options
 # --------------
+set :ssh_options, keys: '~/.ssh/SAKELOG_key_rsa' 
 #  set :ssh_options, {
 #    keys: %w(/home/user_name/.ssh/id_rsa),
 #    forward_agent: false,
