e27ec29a90bb3a17053d2ccc8aa42928f9387022
sasaki kei <sasakei1021@icloud.com>
2020-05-11 15:35:17 +0900

    post-raty

---
+++ db/migrate/20200511044806_add_rate_to_micropost.rb
@@ -0,0 +1,5 @@
+class AddRateToMicropost < ActiveRecord::Migration[5.2]
+  def change
+    add_column :microposts, :rate, :float, null: false, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_060136) do
+ActiveRecord::Schema.define(version: 2020_05_11_044806) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_04_28_060136) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "rate", default: 0.0, null: false
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
