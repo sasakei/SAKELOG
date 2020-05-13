f613b3587a5d3724d18a62b6530b3a02dd8b64c1
sasaki kei <sasakei1021@icloud.com>
2020-05-13 14:35:31 +0900

    profile-remake

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
-    # @favorites = @user.bookmark_microposts.paginate(page: params[:page]).includes(:user)
+    @favorites = @user.bookmark_microposts.paginate(page: params[:page]).includes(:user)
   end
 
 
