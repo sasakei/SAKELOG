26eb2e6c6f47b74f10ec9aff8c19ccf6d72ca1d9
sasaki kei <sasakei1021@icloud.com>
2020-05-23 16:47:44 +0900

    add circle-ci

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -1,51 +1,36 @@
-# require 'rails_helper'
-#
-# describe PostsController do
-#   describe 'GET #index' do
-#     it '投稿が新着順に表示されること' do
-#       posts = create_list(:post, 3, :with_images)
-#       get :index
-#       expect(assigns(:posts)).to match(posts.sort{ |a, b| b.created_at <=> a.created_at } )
-#     end
-#
-#     it 'indexテンプレートをレンダリングすること' do
-#       get :index
-#       expect(response).to render_template :index
-#     end
-#   end
-#
-#   describe 'GET #show' do
-#     it 'リクエストされたpostのパラメータが@postに割り当てられていること' do
-#       post = create(:post, :with_images)
-#       get :show, params: { id: post }
-#       expect(assigns(:post)).to eq post
-#     end
-#
-#     it 'showテンプレートをレンダリングすること' do
-#       post = create(:post, :with_images)
-#       get :show, params: { id: post }
-#       expect(response).to render_template :show
-#     end
-#   end
-#
-#   describe 'GET #new' do
-#     it 'newテンプレートをレンダリングすること' do
-#       get :new
-#       expect(response).to render_template :new
-#     end
-#   end
-#
-#   describe 'GET #edit' do
-#     it 'リクエストされたpostのパラメータが@postに割り当てられていること' do
-#       post = create(:post, :with_images)
-#       get :edit, params: { id: post }
-#       expect(assigns(:post)).to eq post
-#     end
-#
-#     it 'editテンプレートをレンダリングすること' do
-#       post = create(:post, :with_images)
-#       get :edit, params: { id: post }
-#       expect(response).to render_template :edit
-#     end
-#   end
-# end
+require 'rails_helper'
+
+describe MicropostsController do
+  describe 'GET #index' do
+    # it '投稿が新着順に表示されること' do
+    # end
+
+    it 'indexテンプレートをレンダリングすること' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    # it 'リクエストされたmicropostのパラメータが@micropostに割り当てられていること' do
+    # end
+    #
+    # it 'showテンプレートをレンダリングすること' do
+    # end
+  end
+
+  describe 'GET #new' do
+    it 'newテンプレートをレンダリングすること' do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'リクエストされたmicropostのパラメータが@micropostに割り当てられていること' do
+    end
+
+    # it 'editテンプレートをレンダリングすること' do
+    # end
+  end
+end
