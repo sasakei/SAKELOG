a153f5b2572cad890941ac5d5ddb9e12753d3332
sasaki kei <sasakei1021@icloud.com>
2020-05-27 17:10:58 +0900

    ransack-search

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -1,35 +1,15 @@
 require 'rails_helper'
 
 describe MicropostsController do
-  describe 'GET #/' do
-    # it '投稿が新着順に表示されること' do
-    # end
-    # it 'indexテンプレートをレンダリングすること' do
-    #   get :microposts
-    #   expect(response).to render_template :index
-    # end
+  describe 'GET #index' do
   end
 
   describe 'GET #show' do
-    # it 'リクエストされたmicropostのパラメータが@micropostに割り当てられていること' do
-    # end
-    #
-    # it 'showテンプレートをレンダリングすること' do
-    # end
   end
 
   describe 'GET #new' do
-    it 'newテンプレートをレンダリングすること' do
-      get :new
-      expect(response).to render_template :new
-    end
   end
 
   describe 'GET #edit' do
-    it 'リクエストされたmicropostのパラメータが@micropostに割り当てられていること' do
-    end
-
-    # it 'editテンプレートをレンダリングすること' do
-    # end
   end
 end
